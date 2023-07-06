987e9aa2863ca341e460dc2e0df2624cd36e6941
leosun <leosunsl@outlook.com>
2023-07-06 15:01:22 +0800

    ceedling test for bcr_hpi

---
+++ test/test_bcr_hpi.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "bcr_hpi.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_bcr_hpi_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement bcr_hpi");
+}
+
+#endif // TEST
