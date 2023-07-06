e94fb1572dc85859b5ba579392c288c7ad05f4f5
leosun <leosunsl@outlook.com>
2023-07-06 15:43:09 +0800

    a few silly tests

---
+++ test/platform_i2c.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+int32_t platform_i2c_write(void *, uint16_t, const uint8_t *, uint16_t);
+int32_t platform_i2c_read(void *, uint16_t, uint8_t *, uint16_t);
--- test/test_bcr_hpi.c
+++ test/test_bcr_hpi.c
@@ -1,20 +1,33 @@
 #ifdef TEST
 
 #include "unity.h"
-
+#include "mock_platform_i2c.h"
 #include "bcr_hpi.h"
+
+ctx_t platform_ctx;
 
 void setUp(void)
 {
+    platform_ctx.write_reg = platform_i2c_write;
+    platform_ctx.read_reg = platform_i2c_read;
 }
 
 void tearDown(void)
 {
 }
 
-void test_bcr_hpi_NeedToImplement(void)
+void test_bcr_hpi_DeviceModeGet(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement bcr_hpi");
+    uint8_t mode = 0;
+    platform_i2c_read_ExpectAndReturn(NULL, DEVICE_MODE_REG, &mode, 1, 0);
+    TEST_ASSERT_EQUAL(0, cypd3177_get_device_mode(&platform_ctx, &mode));
+}
+
+void test_bcr_hpi_GetDeviceId(void)
+{
+    uint16_t id = 0;
+    platform_i2c_read_ExpectAndReturn(NULL, SILICON_ID_REG, &id, 2, 0);
+    TEST_ASSERT_EQUAL(0, cypd3177_get_silicon_id(&platform_ctx, &id));
 }
 
 #endif // TEST
